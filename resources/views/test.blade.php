eb8be1009ad6d2cf6e7ece72733843effbd038f9
lantisse <lantisse@192.168.5.101>
2016-11-04 19:02:51 +0700

    This is test page

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,7 +6,7 @@
             <!-- Box -->
             <div class="box box-primary">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Randomly Generated Tasks</h3>
+                    <h3 class="box-title">This is test page</h3>
                     <div class="box-tools pull-right">
                         <button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>
                         <button class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove"><i class="fa fa-times"></i></button>
